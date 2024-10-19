afe55d60599d46e409ed2b7140714c6531946dc7
Charlie-learner <c84515858@gmail.com>
2024-10-19 22:29:57 +0800

    HomePage+NavBar+AppFooter

---
+++ .history/src/App_20241019215357.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <router-view />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    NavBar,
+    AppFooter
+  }
+}
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -7,4 +7,3 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 createApp(App).use(router).mount('#app')
-
