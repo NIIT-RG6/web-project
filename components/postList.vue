3d4bf9b3e7aaf2213f5add507e28a803f002d1ac
woiydan git <woiydan@qq.com>
2024-10-23 17:01:01 +0800

    1.1.3changed the image url from cdn

--- components/postList.vue
+++ components/postList.vue
@@ -15,7 +15,7 @@ export default {
     return {
       posts: [
         { id: 1, title: '海口CBD的日落居然那么美', description: '国兴大道大英山CBD已经初具规模，未来待海南中心建成，海口的天际线将会更有层次感!', 
-        image1: 'https://mp-c3036ca5-d4f1-4495-acd5-cf521789bd80.cdn.bspapp.com/static/img1.jpg',image2:'https://mp-c3036ca5-d4f1-4495-acd5-cf521789bd80.cdn.bspapp.com/static/img2.jpg' ,
+        image1: 'https://sns-webpic-qc.xhscdn.com/202410231657/33e44121ff68ac39f6d87c123095dde0/1040g0083153m4k3uh4005n8or554ebpvasmo368!nd_prv_wgth_webp_3',image2:'https://sns-webpic-qc.xhscdn.com/202410231657/33e44121ff68ac39f6d87c123095dde0/1040g0083153m4k3uh4005n8or554ebpvasmo368!nd_prv_wgth_webp_3' ,
         comments: [
         {name: '黎小喵', content: '最近的天气真的挺好的'},
         {name: '💫Vincent 游北卡', content: "The weather's been really nice lately."},
